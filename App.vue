c6db867214a37008f509645eee2c8bfab7f3f8e8
davay <davay@defucc.me>
2025-09-10 19:24:45 +0700

    feat: intro

--- App.vue
+++ App.vue
@@ -11,7 +11,7 @@ const currentGlobe = ref(globes[0])
 
 const { sampler, presets, currentPreset, loading, progress, audioBuffer, params, selectPreset, loadFile, loadUrl, triggerAttack, triggerRelease } = useSampler()
 
-const { activeNotes, midiNote, guessChords } = useMIDI()
+const { activeNotes, midiNote, guessChords, inputs } = useMIDI()
 
 watch(midiNote, n => {
   if (n.velocity > 0) triggerAttack(Midi(n.number).toNote(), "+0.000000001", n.velocity)
@@ -68,9 +68,13 @@ function animate(time) {
 
 onMounted(() => {
   rafId = requestAnimationFrame(animate)
+  novis.value = window.location.hash == '#novis'
 })
 
 onUnmounted(() => cancelAnimationFrame(rafId))
+
+const started = ref(false)
+const novis = ref(false)
 
 </script>
 
@@ -78,20 +82,38 @@ onUnmounted(() => cancelAnimationFrame(rafId))
 .flex.flex-col.items-center.w-full.h-100svh.justify-between.text-white
   img.z-10.w-90.mt-12.mb-8.z-200(src="/logo_la_mer_white.svg")
 
-  .flex.flex-wrap.gap-2.justify-center.z-200
-    .p-4.op-75.hover-op-85.active-op-100.cursor-pointer.text-center.tracking-wider.transition-700.variable-text.whitespace-nowrap.select-none(v-for="globe in globes" :key="globe.name" @click="currentGlobe = globe; selectPreset(globe.preset)" :class="{'active': currentGlobe.name == globe.name}" style="contain: layout;") {{globe.name}} 
+  template(v-if="started")
+    .flex.flex-wrap.gap-2.justify-center.z-200.flex-1
+      .p-4.op-75.hover-op-85.active-op-100.cursor-pointer.text-center.tracking-wider.transition-700.variable-text.whitespace-nowrap.select-none(v-for="globe in globes" :key="globe.name" @click="currentGlobe = globe; selectPreset(globe.preset)" :class="{'active': currentGlobe.name == globe.name}" style="contain: layout;") {{globe.name}} 
 
-  .p-4.flex.flex-wrap.gap-8.justify-center.items-center.w-full.flex-auto.z-10
-    .flex.items-center.gap-6.flex-wrap.w-full.justify-center
-      .flex.text-center.relative.justify-center.items-start.flex-col(style="perspective: 1000px; transform-style: preserve-3d;")
-        GradientCircle(:size="400"
-          :style="{transform: `scale(${1+turb.r}) rotateZ(${turb.z*120}deg) rotateX(${turb.x*15}deg) rotateY(${turb.y*15}deg)`}"
-          :active="!!active"
-          v-bind="globeWithNotes")
+    .p-4.flex.flex-wrap.gap-8.justify-center.items-center.w-full.flex-auto.z-10(v-if="!novis") {{novis}}
+      .flex.items-center.gap-6.flex-wrap.w-full.justify-center
+        .flex.text-center.relative.justify-center.items-start.flex-col(style="perspective: 1000px; transform-style: preserve-3d;")
+          GradientCircle(:size="400"
+            :style="{transform: `scale(${1+turb.r}) rotateZ(${turb.z*120}deg) rotateX(${turb.x*25}deg) rotateY(${turb.y*25}deg)`}"
+            :active="!!active"
+            v-bind="globeWithNotes")
 
 
-  .p-2.flex.flex-wrap.gap-2.absolute.bottom-2.z-100
-    .p-2.op-70.font-thin(v-for="(chord,c) in guessChords" :key="c") {{chord}}
+    .p-2.flex.flex-wrap.gap-2.absolute.bottom-2.z-100
+      .p-2.op-70.font-thin(v-for="(chord,c) in guessChords" :key="c") {{chord}}
+
+  .flex.flex-col.gap-4.flex-1.max-w-55ch.leading-loose.text-center(v-else)
+    .text-2xl Love. Share. Play. {{novis}}
+    p Turn on your speakers, then connect TouchMe or press (Z-M and Q-P parts) keys on your computer keyboard to start playing. This website is optimized for desktop only.
+    a.underline(href="https://www.youtube.com/@PlayTronica" target="_blank") Watch this tutorial to find out more.
+    p Now you can visualise notes and experiment with smooth and sassy sounds. Our TouchMe devices can turn a hug into music, or a kiss into a musical note.
+    p The musical content presented on this site is for non-commercial use only. If your computer is slow, try <a class="underline" href="#novis" @click="novis = true;started = true">playing without visuals</a>.
+    .flex.flex-col.gap-4.items-center(v-if="Object.keys(inputs).length") 
+      .text-lg Connected devices: 
+      .flex.justify-center
+        .p-2.text-lg.bg-dark-100.rounded-lg(v-for="input in inputs" :key="input") {{input.name}}
+    button.button.relative.overflow-hidden.flex.items-center.justify-center(@click="started = true")
+      .absolute.top-9.text-2xl.z-100 START
+      img.invert.grayscale.w-60.h-30(src="/swoosh.svg")
+    .text-sm Please email <a class="underline" href="mailto:support@playtronica.com">support@playtronica.com</a> for assistance.  We are here for you.
+
+    
 </template>
 
 <style>
